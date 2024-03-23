63fd50257906d9d7eb0352fc3959ae1b1042307d
Elder-Ferreira <ferreira3.elder@gmail.com>
2024-03-22 23:16:34 -0300

    Editado páginas Index HomePage e Index CadastroProdutos

--- codigo-fonte/MagnificoPonto/MagnificoPonto/obj/Debug/net6.0/MagnificoPonto.AssemblyInfo.cs
+++ codigo-fonte/MagnificoPonto/MagnificoPonto/obj/Debug/net6.0/MagnificoPonto.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagnificoPonto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb35e1b2e1761bffa7a2dc0b33ad4967b6c26a92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71239e45f1a6feebb626819d1701436b78827771")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagnificoPonto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagnificoPonto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
