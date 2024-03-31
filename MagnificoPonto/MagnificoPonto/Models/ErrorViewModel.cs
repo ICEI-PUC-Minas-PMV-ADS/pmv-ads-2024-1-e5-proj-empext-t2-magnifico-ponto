9a4d2c656836336dda36e6b337399f8df7bcafdc
Matheus Fernandes <mfernandesr97@gmail.com>
2024-03-31 11:26:01 -0300

    Criando projeto Magnifico Ponto

---
+++ MagnificoPonto/MagnificoPonto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MagnificoPonto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MagnificoPonto/MagnificoPonto/Program.cs
@@ -0,0 +1,18 @@
+namespace MagnificoPonto;
+
+public class Program 
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args)
+            .Build()
+            .Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseStartup < Startup >();
+        });
+}
