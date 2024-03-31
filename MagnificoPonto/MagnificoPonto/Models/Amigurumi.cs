9758574b5f9e9cfd03ade205a048101b855810e9
Matheus Fernandes <mfernandesr97@gmail.com>
2024-03-31 12:51:46 -0300

    Criando as classes de Dominio Categoria e Amigurumi

---
+++ MagnificoPonto/MagnificoPonto/Models/Amigurumi.cs
@@ -0,0 +1,27 @@
+﻿namespace MagnificoPonto.Models
+{
+    public class Amigurumi
+    {
+        public string AmigurumiId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string DescricaoCurta { get; set; }
+
+        public string DescricaoDetalhada { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public string ImagemUrl { get; set; }
+
+        public string ImagemThumbnailUrl { get; set; }
+
+        public bool AmigurumiLancamento { get; set; }
+
+        public bool EmEstoque { get; set; }
+
+        public int CategoriaId { get; set; }
+
+        public virtual Categoria Categoria { get; set;}
+    }
+}
---
+++ MagnificoPonto/MagnificoPonto/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿namespace MagnificoPonto.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+
+        public string CategoriaNome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public List<Amigurumi> Amigurumis { get; set; }
+    }
+}
