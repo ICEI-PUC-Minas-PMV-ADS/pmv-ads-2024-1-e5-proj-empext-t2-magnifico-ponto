12e326620fd7b794f7f3a58df88ae020aefdc619
Matheus Fernandes <mfernandesr97@gmail.com>
2024-04-10 00:15:59 -0300

    Implementando opadrão Repository para Amigurumis

---
+++ MagnificoPonto/MagnificoPonto/Repositories/AmigurumiRepository.cs
@@ -0,0 +1,27 @@
+﻿using MagnificoPonto.Context;
+using MagnificoPonto.Models;
+using MagnificoPonto.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagnificoPonto.Repositories
+{
+    public class AmigurumiRepository : IAmigurumiRepository
+    {
+        private readonly AppDbContext _context;
+        public AmigurumiRepository(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+
+        public IEnumerable<Amigurumi> Amigurumis => _context.Amigurumis.Include(c=> c.Categoria);
+
+        public IEnumerable<Amigurumi> AmigurumiLancamento => _context.Amigurumis
+            .Where(a => a.AmigurumiLancamento)
+            .Include(c=>c.Categoria);
+
+        public Amigurumi GetAmigurumiById(int amigurumiId)
+        {
+            return _context.Amigurumis.FirstOrDefault(a => a.AmigurumiId == amigurumiId);
+        }
+    }
+}
---
+++ MagnificoPonto/MagnificoPonto/Repositories/Interfaces/IAmigurumiRepository.cs
@@ -0,0 +1,13 @@
+﻿using MagnificoPonto.Models;
+
+namespace MagnificoPonto.Repositories.Interfaces
+{
+    public interface IAmigurumiRepository
+    {
+        IEnumerable <Amigurumi> Amigurumis { get; }
+
+        IEnumerable <Amigurumi> AmigurumiLancamento { get; }
+
+        Amigurumi GetAmigurumiById (int amigurumiId);
+    }
+}
