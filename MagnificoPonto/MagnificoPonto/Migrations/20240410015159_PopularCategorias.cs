6bb3149223246104828bd1701c622eb6995bd960
Matheus Fernandes <mfernandesr97@gmail.com>
2024-04-09 23:02:04 -0300

    Populando tabela Categorias (Animais, Personagens, Pessoas)

---
+++ MagnificoPonto/MagnificoPonto/Migrations/20240410015159_PopularCategorias.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagnificoPonto.Migrations
+{
+    public partial class PopularCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome, Descricao)" + 
+                "VALUES('Animal', 'Amigurumi de animais')");
+
+            migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome, Descricao)" +
+                "VALUES('Personagens', 'Amigurumi de personagens infantis')");
+
+            migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome, Descricao)" +
+                "VALUES('Pessoas', 'Amigurumi de pessoas')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categorias");
+        }
+    }
+}
