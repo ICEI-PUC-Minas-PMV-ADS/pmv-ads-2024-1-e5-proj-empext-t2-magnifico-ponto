c8b4841efa0b85895b6d5dd9cad3dc63986664df
Matheus Fernandes <mfernandesr97@gmail.com>
2024-04-10 22:41:47 -0300

    Criando o Controller e a View para exibir os amigurumis

---
+++ MagnificoPonto/MagnificoPonto/Controllers/AmigurumiController.cs
@@ -0,0 +1,21 @@
+﻿using MagnificoPonto.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagnificoPonto.Controllers
+{
+    public class AmigurumiController : Controller
+    {
+        private readonly IAmigurumiRepository _amigurumiRepository;
+
+        public AmigurumiController(IAmigurumiRepository amigurumiRepository)
+        {
+            _amigurumiRepository = amigurumiRepository;
+        }
+
+        public IActionResult List()
+        {
+            var amigurumis = _amigurumiRepository.Amigurumis;
+            return View(amigurumis);
+        }
+    }
+}
