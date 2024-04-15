075167705b19bc3a40d34ae24bfffac522954dc0
Matheus Fernandes <103971912+mfernandesr@users.noreply.github.com>
2024-04-15 19:22:24 -0300

    Finalizando o controlador do carrinho de compras

--- MagnificoPonto/MagnificoPonto/Controllers/CarrinhoCompraController.cs
+++ MagnificoPonto/MagnificoPonto/Controllers/CarrinhoCompraController.cs
@@ -1,5 +1,6 @@
 ﻿using MagnificoPonto.Models;
 using MagnificoPonto.Repositories.Interfaces;
+using MagnificoPonto.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagnificoPonto.Controllers
@@ -7,17 +8,51 @@ namespace MagnificoPonto.Controllers
     public class CarrinhoCompraController : Controller
     {
         private readonly IAmigurumiRepository _amigurumiRepository;
-        private readonly CarrinhoCompra carrinhoCompra;
+        private readonly CarrinhoCompra _carrinhoCompra;
 
         public CarrinhoCompraController(IAmigurumiRepository amigurumiRepository, CarrinhoCompra carrinhoCompra)
         {
             _amigurumiRepository = amigurumiRepository;
-            this.carrinhoCompra = carrinhoCompra;
+            _carrinhoCompra = carrinhoCompra;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+            _carrinhoCompra.CarrinhoCompraItens = itens;
+
+            var carrinhoCompraVM = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+            };
+            return View(carrinhoCompraVM);
+        }
+
+        public IActionResult AdicionarItemNoCarrinhoCompra (int amigurumiId)
+        {
+            var amigurumiSelecionado = _amigurumiRepository.Amigurumis
+                                       .FirstOrDefault(p=> p.AmigurumiId == amigurumiId);
+
+            if (amigurumiSelecionado != null)
+            {
+                _carrinhoCompra.AdicionarAoCarrinho(amigurumiSelecionado);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult RemoverItemNoCarrinhoCompra(int amigurumiId)
+        {
+            var amigurumiSelecionado = _amigurumiRepository.Amigurumis
+                                       .FirstOrDefault(p => p.AmigurumiId == amigurumiId);
+
+            if (amigurumiSelecionado != null)
+            {
+                _carrinhoCompra.RemoverDoCarrinho(amigurumiSelecionado);
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
