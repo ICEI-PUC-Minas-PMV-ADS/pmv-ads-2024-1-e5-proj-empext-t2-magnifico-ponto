731ff7bd2f95179cb32a6095f307b0550989f667
Matheus Fernandes <103971912+mfernandesr@users.noreply.github.com>
2024-04-16 21:42:14 -0300

    Implementando pagina de contatos
    Ajustar:
    - estilizar mais a pagina
    - trocar as imagens por ícones (se necessario)

---
+++ MagnificoPonto/MagnificoPonto/Controllers/ContatoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MagnificoPonto.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MagnificoPonto/MagnificoPonto/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MagnificoPonto.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string Endereco { get; set; }
+
+        public string Conteudo { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", "mailto:" + Endereco);
+            output.Content.SetContent(Conteudo);
+        }
+    }
+}
