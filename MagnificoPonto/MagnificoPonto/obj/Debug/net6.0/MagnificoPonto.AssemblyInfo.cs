79157e384a5e002c405d0c9282a173d8308bd218
Matheus Fernandes <103971912+mfernandesr@users.noreply.github.com>
2024-04-27 15:56:51 -0300

    Layout alternativo
    Ajustar:
    - icones
    - imagens
    - fontes/cores
    (procurar layouts no bootstrap)

--- MagnificoPonto/MagnificoPonto/obj/Debug/net6.0/MagnificoPonto.AssemblyInfo.cs
+++ MagnificoPonto/MagnificoPonto/obj/Debug/net6.0/MagnificoPonto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagnificoPonto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31e0d495ddf6299c68030e00fa2baad6a5e25da4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8186065004d8ad296ce0c1adaf922dfe880e4db6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagnificoPonto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagnificoPonto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
