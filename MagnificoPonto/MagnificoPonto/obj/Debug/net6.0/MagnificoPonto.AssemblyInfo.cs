bf65f61777bc0843f09bcf80afd775895f766d91
Matheus Fernandes <103971912+mfernandesr@users.noreply.github.com>
2024-05-11 23:21:51 -0300

    Adicionando migration PedidoDetalhes

--- MagnificoPonto/MagnificoPonto/obj/Debug/net6.0/MagnificoPonto.AssemblyInfo.cs
+++ MagnificoPonto/MagnificoPonto/obj/Debug/net6.0/MagnificoPonto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagnificoPonto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8186065004d8ad296ce0c1adaf922dfe880e4db6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagnificoPonto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagnificoPonto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
