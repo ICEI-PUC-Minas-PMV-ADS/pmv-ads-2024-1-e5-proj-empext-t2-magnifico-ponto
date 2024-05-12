d9588219739e548c369549642a9e3da7a24e80bd
Elder-Ferreira <ferreira3.elder@gmail.com>
2024-05-12 16:18:33 -0300

    Corrigido integração contatos/Gereciamento de mensagens

--- codigo-fonte/MagnificoPonto/MagnificoPonto/obj/Debug/net6.0/MagnificoPonto.AssemblyInfo.cs
+++ codigo-fonte/MagnificoPonto/MagnificoPonto/obj/Debug/net6.0/MagnificoPonto.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagnificoPonto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3bea35be4be52c2c653f6f440cc4ea2b672a46d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ea3418f08aaf72ba728811ee2ecde0fc8102d55")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagnificoPonto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagnificoPonto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
