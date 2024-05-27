d8a8314fd9100406f95cf8afef135b7bac113d82
Franciele Félix <103470377+FrancieleFelix@users.noreply.github.com>
2024-05-27 17:39:03 -0300

    API correios - part 3

---
+++ codigo-fonte/MagnificoPonto/MagnificoPonto/Services/Models/PrazoResponse.cs
@@ -0,0 +1,21 @@
+﻿namespace Correios.Demo.Services.Models
+{
+    public class PrazoResponse
+    {
+        public string coProduto { get; set; }
+
+        public string nuRequisicao { get; set; }
+
+        public int prazoEntrega { get; set; }
+
+        public string dataMaxima { get; set; }
+
+        public string txErro { get; set; }
+
+        public string entregaDomiciliar { get; set; }
+
+        public string entregaSabado { get; set; }
+
+        public string msgPrazo { get; set; }
+    }
+}
