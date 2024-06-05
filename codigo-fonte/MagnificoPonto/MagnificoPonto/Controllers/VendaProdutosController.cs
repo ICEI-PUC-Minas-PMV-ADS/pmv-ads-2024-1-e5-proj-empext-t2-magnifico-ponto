0d9a8046aea2b4b8bca989a7e86a8128c1f4fbf8
Elder-Ferreira <ferreira3.elder@gmail.com>
2024-06-05 18:01:42 -0300

    Criação das controllers e views para VendaProdutos

---
+++ codigo-fonte/MagnificoPonto/MagnificoPonto/Controllers/VendaProdutosController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MagnificoPonto.Data;
+using MagnificoPonto.Models;
+
+namespace MagnificoPonto.Controllers
+{
+    public class VendaProdutosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VendaProdutosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: VendaProdutos
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.VendaProdutos.ToListAsync());
+        }
+
+        // GET: VendaProdutos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.VendaProdutos == null)
+            {
+                return NotFound();
+            }
+
+            var vendaProdutosModel = await _context.VendaProdutos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vendaProdutosModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(vendaProdutosModel);
+        }
+
+        // GET: VendaProdutos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VendaProdutos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Quantidade,Personalizacao,CriadoEm")] VendaProdutosModel vendaProdutosModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(vendaProdutosModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vendaProdutosModel);
+        }
+
+        // GET: VendaProdutos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.VendaProdutos == null)
+            {
+                return NotFound();
+            }
+
+            var vendaProdutosModel = await _context.VendaProdutos.FindAsync(id);
+            if (vendaProdutosModel == null)
+            {
+                return NotFound();
+            }
+            return View(vendaProdutosModel);
+        }
+
+        // POST: VendaProdutos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Quantidade,Personalizacao,CriadoEm")] VendaProdutosModel vendaProdutosModel)
+        {
+            if (id != vendaProdutosModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(vendaProdutosModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VendaProdutosModelExists(vendaProdutosModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vendaProdutosModel);
+        }
+
+        // GET: VendaProdutos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.VendaProdutos == null)
+            {
+                return NotFound();
+            }
+
+            var vendaProdutosModel = await _context.VendaProdutos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vendaProdutosModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(vendaProdutosModel);
+        }
+
+        // POST: VendaProdutos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.VendaProdutos == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.VendaProdutos'  is null.");
+            }
+            var vendaProdutosModel = await _context.VendaProdutos.FindAsync(id);
+            if (vendaProdutosModel != null)
+            {
+                _context.VendaProdutos.Remove(vendaProdutosModel);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VendaProdutosModelExists(int id)
+        {
+          return _context.VendaProdutos.Any(e => e.Id == id);
+        }
+    }
+}
--- codigo-fonte/MagnificoPonto/MagnificoPonto/obj/Debug/net6.0/MagnificoPonto.AssemblyInfo.cs
+++ codigo-fonte/MagnificoPonto/MagnificoPonto/obj/Debug/net6.0/MagnificoPonto.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagnificoPonto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9b1b607d3e4e83770d9d4414d325c6e05d2c476")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cc8a57c9997750f7ccada3d8c1cca0a8190d05f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagnificoPonto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagnificoPonto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
