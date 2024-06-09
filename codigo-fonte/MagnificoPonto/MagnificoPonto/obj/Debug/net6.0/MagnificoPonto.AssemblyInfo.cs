6b7ae6373a1c3111189b03ac98c1157821e942a9
Elder-Ferreira <ferreira3.elder@gmail.com>
2024-06-09 01:12:00 -0300

    Alteração da classe IndexVendaProdutos

--- codigo-fonte/MagnificoPonto/MagnificoPonto/obj/Debug/net6.0/MagnificoPonto.AssemblyInfo.cs
+++ codigo-fonte/MagnificoPonto/MagnificoPonto/obj/Debug/net6.0/MagnificoPonto.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagnificoPonto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd88445faee43eb8f023d05c2330466a7c9b6857")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a35025165334627c4bea667f6718186ff7c69d8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagnificoPonto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagnificoPonto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
